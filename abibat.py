1bccd69fa8b86d4b4a9d8c9a3d48ceb5d6b0402c
Abibat <habybgal@gmail.com>
2021-01-06 22:39:58 +0100

    Added list of fruits

---
+++ abibat.py
@@ -0,0 +1,6 @@
+#practice a for loop
+fruits = ["banana", "pawpaw","orange","grape","guava","apple","water melon","lime","beetroot","cashew","cherry"]
+for fruit in fruits:
+    if len(fruit) > 5:
+        print(fruit)
+        continue
